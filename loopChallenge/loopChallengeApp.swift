58df8372c9e80c506280c210a3c14a4e4d35e56c
RJaniec <rafael.janiec@outlook.com>
2022-06-29 11:32:49 +0200

    Initial Commit

---
+++ loopChallenge/loopChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopChallengeApp.swift
+//  loopChallenge
+//
+//  Created by Rafał Janiec on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct loopChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
